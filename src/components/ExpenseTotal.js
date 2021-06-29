0aa8ada605f5dcc0b4037a06fb787736445d6439
Colin Dorman <csdorman@gmail.com>
2021-06-29 14:58:27 -0400

    App creation, dummy layout, create expense item and list

---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseTotal = () => {
+    return (
+        <div className='aler alert-primary'>
+            <span>Spent so far: $1,500</span>
+        </div>
+    )
+}
+
+export default ExpenseTotal;
