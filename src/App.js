40de37c70584cab61fd0db691bbcb2794869ba4f
Colin Dorman <csdorman@gmail.com>
2021-07-01 16:34:40 -0400

    Added AppContext.js

--- src/App.js
+++ src/App.js
@@ -5,35 +5,39 @@ import Remaining from './components/Remaining'
 import ExpenseTotal from './components/ExpenseTotal'
 import ExpenseList from './components/ExpenseList'
 import AddExpenseForm from './components/AddExpenseForm'
+import { AppProvider } from './context/AppContext'
 
 const App = () => {
   return (
-    <div className='container'>
-      <h1 className='mt-3'>My Budget Planner and Tracker</h1>
-      <div className='row mt-3'>
-        <div className='col-sm'>
-          <Budget />  
-        </div>
-        <div className="col-sm">
-          <Remaining />
-        </div>
-        <div className="col-sm">
-          <ExpenseTotal />
-        </div>
-      </div>
-      <h3 className="mt-3">Itemized Expenses</h3>
-      <div className="row mt-3">
-        <div className="col-sm">
-          <ExpenseList />
-        </div>
-      </div>
-      <h3 className="mt-3">Add Itemized Expense</h3>
-        <div className="row mt-3">
+    <AppProvider>
+      <div className='container'>
+        <h1 className='mt-3'>My Budget Planner and Tracker</h1>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <Budget />  
+          </div>
           <div className="col-sm">
-            <AddExpenseForm />
+            <Remaining />
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal />
           </div>
         </div>
-    </div>
+        <h3 className="mt-3">Itemized Expenses</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+        <h3 className="mt-3">Add Itemized Expense</h3>
+          <div className="row mt-3">
+            <div className="col-sm">
+              <AddExpenseForm />
+            </div>
+          </div>
+      </div>
+    </AppProvider>
+    
   )
 }
 
