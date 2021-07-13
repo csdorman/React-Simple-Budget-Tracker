7e71d9d5bd670516f63a76cfb20ca47f734c6d9d
Colin Dorman <csdorman@gmail.com>
2021-07-13 13:48:21 -0400

    Add useState in import

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import ViewBudget from './ViewBudget'
 import EditBudget from './EditBudget'
 import { AppContext } from '../context/AppContext'
