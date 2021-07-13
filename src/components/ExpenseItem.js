df36faad050f856ad75505182c4d21acfc55e806
Colin Dorman <csdorman@gmail.com>
2021-07-13 13:11:23 -0400

    Added delete functionality

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,18 @@
-import { props } from 'bluebird'
-import React from 'react'
+//import { props } from 'bluebird'
+import React, {useContext} from 'react'
 import { TiDelete } from 'react-icons/ti'
+import { AppContext } from '../context/AppContext'
 
 const ExpenseItem = (props) => {
+    const {dispatch} = useContext(AppContext)
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        })
+    }
+
     return (
         <li className='list-group-item d-flex justify-content-between align-items-center'>
             {props.name}
@@ -10,7 +20,7 @@ const ExpenseItem = (props) => {
                 <span className='badge bg-primary badge-pill mr-3'>
                     ${props.cost}
                 </span>
-                <TiDelete size='1.5em'></TiDelete>
+                <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
             </div>
         </li>
     )
