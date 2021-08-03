c0387be4a6bfef725557776ca4a51bc5890ab8b5
Colin Dorman <csdorman@gmail.com>
2021-08-03 14:22:18 -0400

    Added search functionality to ExpenseList

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,17 +1,37 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { AppContext } from '../context/AppContext'
 import ExpenseItem from './ExpenseItem'
 
 const ExpenseList = () => {
     const { expenses } = useContext(AppContext)
+    const [filteredExpenses, setFilteredExpenses] = useState(expenses || [])
 
+    useEffect(() => {
+        setFilteredExpenses(expenses)
+    }, [expenses])
+
+    const handleChange = (event) => {
+        const searchResults = expenses.filter((filteredExpense) =>
+            filteredExpense.name.toLowerCase().includes(event.target.value)
+            )
+            setFilteredExpenses(searchResults)
+    }
 
     return (
-        <ul className='list-group'>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
-        </ul>
+        <>
+            <input
+                type='text'
+                class='form-control mb-2 mr-sm-2'
+                placeholder='Search your expenses...'
+                onChange={handleChange}>
+
+            </input>
+            <ul className='list-group'>
+                {filteredExpenses.map((expense) => (
+                    <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
+                ))}
+            </ul>
+        </>
     )
 }
 
